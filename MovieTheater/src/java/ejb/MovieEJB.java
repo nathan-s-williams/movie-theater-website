10e5ed5943cf523554009f5a6b6238242d486a7c
Nathan Williams <nate88williams@gmail.com>
2022-07-20 10:54:19 -0500

    Created separate EJB's for each entity to act as DAO classes.

---
+++ MovieTheater/src/java/ejb/MovieEJB.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+import entity.Movie;
+import entity.Theater;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+/**
+ *
+ * @author nate
+ */
+@Stateless
+public class MovieEJB {
+
+    @PersistenceContext(unitName = "MoviePU")
+    private EntityManager em;
+    
+    public List<Movie> findAllMovies(String movieId) {
+        return em.createNamedQuery("Movie.findAllMovies", Movie.class)
+                .getResultList();
+    }
+    
+    public Movie findByMovieId(String movieId) {
+        return em.createNamedQuery("Movie.findByMovieId", Movie.class)
+                .setParameter("movieId", movieId)
+                .getSingleResult();
+    }
+    
+    public Movie findByTitle(String name) {
+        return em.createNamedQuery("Movie.findByTitle", Movie.class)
+                .setParameter("title", name)
+                .getSingleResult();
+    }
+    
+    public Movie findByDescription(String description) {
+        return em.createNamedQuery("Movie.findByDescription", Movie.class)
+                .setParameter("description", description)
+                .getSingleResult();
+    }
+    
+    public List<Theater> findTheatersByMovie(String movieId) {
+        return em.createNamedQuery("Movie.findTheatersByMovie", Theater.class)
+                .setParameter("movieId", movieId)
+                .getResultList();
+    }
+    
+    //Method used to persist an object.
+    public void persist(Object object) {
+        em.persist(object);
+    }
+}
---
+++ MovieTheater/src/java/ejb/TheaterEJB.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+import entity.Movie;
+import entity.Theater;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+/**
+ *
+ * @author nate
+ */
+@Stateless
+public class TheaterEJB {
+
+    @PersistenceContext(unitName = "TheaterPU")
+    private EntityManager em;
+    
+    public List<Theater> findAllTheaters(String movieId) {
+        return em.createNamedQuery("Theater.findAllTheaters", Theater.class)
+                .getResultList();
+    }
+    
+    public Theater findByTheaterId(String theaterId) {
+        return em.createNamedQuery("Theater.findByTheaterId", Theater.class)
+                .setParameter("theaterId", theaterId)
+                .getSingleResult();
+    }
+    
+    public Theater findByTheaterName(String name) {
+        return em.createNamedQuery("Theater.findByTheaterName", Theater.class)
+                .setParameter("theaterName", name)
+                .getSingleResult();
+    }
+    
+    public List<Theater> findByTheaterZipcode(int zipcode) {
+        if(!ClientValidatorEJB.validateZipcode(zipcode))
+            return null;
+        return em.createNamedQuery("Theater.findByTheaterZipcode", Theater.class)
+                .setParameter("zipcode", zipcode)
+                .getResultList();
+    }
+    
+    public List<Movie> findMoviesByTheater(String theaterId) {
+        return em.createNamedQuery("Theater.findMoviesByTheater", Movie.class)
+                .setParameter("theaterId", theaterId)
+                .getResultList();
+    }
+    
+    //Method used to persist an object.
+    public void persist(Object object) {
+        em.persist(object);
+    }
+}
