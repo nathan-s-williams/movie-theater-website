547028397f0c42ce2528936fec0056152dad53c8
Nathan Williams <nate88williams@gmail.com>
2022-07-23 02:48:02 -0500

    Deleted header Template. Renamed original main.css to index.css and created a new main.css for the rest of the site. Added a javascript file to handle the nav bar and footer.

---
+++ MovieTheater/web/resources/javascript/script.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+const nav = document.getElementById("nav")
+        .innerHTML='<nav class="nav-bar"><div class="title">Welcome to Javango</div><div class="nav-links"><a href="index.xhtml">Home</a><a href="Theaters.xhtml">Theaters</a><a href="Movies.xhtml">Movies</a><a href="AboutUs.xhtml">About Us</a></div></nav>';
+
+const footer = document.getElementById("footer")
+        .innerHTML='<div style="text-align: center">Advanced Java CS4336 Final Project<br />By Nathan Williams and Akito Minosoko</div>';
+
+
+
