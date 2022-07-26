d00d8db72da5e8dcb456986933821c0dbf6e8671
akiminosoko <minosokoa@aol.com>
2022-07-26 15:58:30 -0500

    These changes added the payment.xhtml, selectseat.xhtml, seat.css, and seatbean.java to handle seat selection page and payement page

---
+++ MovieTheater/src/java/bean/SeatBean.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import entity.Movie;
+import entity.Payment;
+import entity.Showtimes;
+import entity.Theater;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author minosokoa
+ */
+@Named(value = "seatBean")
+@RequestScoped
+public class SeatBean {
+
+    /**
+     * Creates a new instance of SeatBean
+     */
+    public SeatBean() {
+    }
+    
+    Movie movie = new Movie();
+    Theater theater = new Theater();
+    Showtimes showtimes = new Showtimes();
+    Payment payment = new Payment();
+    
+    
+    List<String> Listseatings;
+    String[] seatings = new String[8];
+    String stringSeatings;
+    String tempString = "";
+    int numOfChosenSeats;
+    
+    
+    public String[] getSeatings () {
+        return seatings;
+    }
+    
+    public void setSeatings(String[] seatings) {
+        for(int i = 0; i < seatings.length; i++) {
+            System.out.println(seatings[i]);
+            tempString = tempString + seatings[i] + " ";
+        }
+        this.seatings = seatings;
+        stringSeatings = tempString;
+        System.out.println(stringSeatings);
+    }
+    
+    public String testSeatings() {
+        System.out.println("Does it go to testSeatings()??");
+        FacesContext fc = FacesContext.getCurrentInstance();
+        Map<String,String> params = fc.getExternalContext().getRequestParameterMap();
+        String result =  params.get("theSelectedSeats"); 
+        stringSeatings = result;
+        return "TestSeating.xhtml";
+    }
+    
+    
+    public String getStringSeatings() {
+        return stringSeatings; 
+    }
+    
+    public void setStringSeatings(String stringSeatings) {
+        this.stringSeatings = stringSeatings;
+    }
+
+    public int getNumOfChosenSeats() {
+        return numOfChosenSeats;
+    }
+
+    public void setNumOfChosenSeats(int numOfChosenSeats) {
+        this.numOfChosenSeats = numOfChosenSeats;
+    }
+    
+    public void calcNumOfSeats() {
+        int num = this.stringSeatings.length() - this.stringSeatings.replaceAll(" ", "").length();
+        setNumOfChosenSeats(num);
+    }
+    
+    public int calcTotal() {
+        return numOfChosenSeats * 10;
+    }
+    
+    
+}
